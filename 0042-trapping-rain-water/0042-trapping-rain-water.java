eb399b66c5260018fb8cfe0d57dfd29a33a482a9
Vedant Bele <belevedant18@gmail.com>
2025-04-11 05:41:06 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.67%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int result = solution.trap(height);
+        System.out.println("Water trapped: " + result);  // Output should be 6
+    }
+}
